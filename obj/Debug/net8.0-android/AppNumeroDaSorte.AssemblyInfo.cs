22c78c9ced022f1bc1dac4d1998085e3b7b3e207
Lorenzo <lorenzodonato75@Gmail.com>
2025-10-05 20:18:28 -0300

    Add .gitignore and remove ignored files

--- obj/Debug/net8.0-android/AppNumeroDaSorte.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/AppNumeroDaSorte.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppNumeroDaSorte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+74f65bedcf311cf20877a3483d8b329c521e83b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppNumeroDaSorte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppNumeroDaSorte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
